6379a3d766b685a0f8d291b54c8a208750de15bf
Manish Kumar <kmanish00745@gmail.com>
2026-09-15 21:02:22 +0530

    dialog voice att correction

--- src/components/dialog_voice_attendance.py
+++ src/components/dialog_voice_attendance.py
@@ -30,7 +30,7 @@ def voice_attendance_dialog(selected_subject_id):
             if not candidates_dict:
                 st.error('No enrolled students have voice profiles registered')
                 return
-            audio_bytes = audio_data.read()
+            audio_bytes = audio_data.get_value()
 
             detected_scores = process_bulk_audio(audio_bytes, candidates_dict)
             results, attendance_to_log = [],[]
